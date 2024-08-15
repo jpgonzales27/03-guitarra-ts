5dd0248584992cb98ab0038bb338b8997bdd4f5d
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-15 19:03:47 -0400

    Fixeando errores de tipados

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,9 +1,9 @@
 import { useState, useEffect, useMemo } from "react";
 import { db } from "../data/db";
-import type { CartItem } from "../types";
+import type { Guitar, CartItem } from "../types";
 
 export const useCart = () => {
-  const initialCart = (): CartItem => {
+  const initialCart = (): CartItem[] => {
     const localStorageCart = localStorage.getItem("cart");
     return localStorageCart ? JSON.parse(localStorageCart) : [];
   };
@@ -18,7 +18,7 @@ export const useCart = () => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  function addToCart(item) {
+  function addToCart(item: Guitar) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
     if (itemExists >= 0) {
       // existe en el carrito
@@ -27,16 +27,16 @@ export const useCart = () => {
       updatedCart[itemExists].quantity++;
       setCart(updatedCart);
     } else {
-      item.quantity = 1;
-      setCart([...cart, item]);
+      const newItem: CartItem = { ...item, quantity: 1 };
+      setCart([...cart, newItem]);
     }
   }
 
-  function removeFromCart(id) {
+  function removeFromCart(id: Guitar["id"]) {
     setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
   }
 
-  function decreaseQuantity(id) {
+  function decreaseQuantity(id: Guitar["id"]) {
     const updatedCart = cart.map((item) => {
       if (item.id === id && item.quantity > MIN_ITEMS) {
         return {
@@ -49,7 +49,7 @@ export const useCart = () => {
     setCart(updatedCart);
   }
 
-  function increaseQuantity(id) {
+  function increaseQuantity(id: Guitar["id"]) {
     const updatedCart = cart.map((item) => {
       if (item.id === id && item.quantity < MAX_ITEMS) {
         return {
@@ -62,7 +62,7 @@ export const useCart = () => {
     setCart(updatedCart);
   }
 
-  function clearCart(e) {
+  function clearCart() {
     setCart([]);
   }
 
